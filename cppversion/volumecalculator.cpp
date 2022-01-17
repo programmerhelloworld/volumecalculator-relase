bbb4d423044f7729ff0cdb59cf61f713299705c7
Francesco Maresca <97321140+programmerhelloworld@users.noreply.github.com>
2022-01-17 11:04:31 +0100

    version 1.1

--- cppversion/volumecalculator.cpp
+++ cppversion/volumecalculator.cpp
@@ -8,7 +8,7 @@ int main(){
 float a; // side of the cube
 float choose;
 
-cout << "1 to calculate cube volume. 2 to calculate square pyramid volume." << endl;
+cout << "1 to calculate cube volume. 2 to calculate square pyramid volume. 3 to calculate rectangular parallelepiped. " << endl;
 cin >> choose;
 
 
@@ -48,8 +48,31 @@ cout << "The volume of the cube is " << vol << "." << endl;
 	area = side * side;
 	pyvol = (area * height)/3;
 	
-	cout << "The volume of the pyramid is " << pyvol << endl;
+	cout << "The volume of the pyramid is " << pyvol << "." << endl;
 	
+
+} else if (choose == 3){
+float side1;
+float side2;
+float height;
+float parvol;
+
+cout << "Give side one" << endl;
+cin >> side1;
+
+cout << "Give side two" << endl;
+cin >> side2;
+
+cout << "Give the height" << endl;
+cin >> height;
+
+parvol = side1 * side2 * height;
+
+cout << "The volume of the rectangular parallelepiped is " << parvol << "." << endl;
+
+
+
+
 
 }
 
