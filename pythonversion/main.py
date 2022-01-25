7401baf324232635e1adb63d8321d4e1a2c0e42f
Francesco Maresca <97321140+programmerhelloworld@users.noreply.github.com>
2022-01-25 10:36:20 +0100

    Update main.py

--- pythonversion/main.py
+++ pythonversion/main.py
@@ -1,1 +1,2 @@
-
+print("Francesco Maresca, Program written in C++, 2022.")
+choose = input("1 to calculate cube volume. 2 to calculate square pyramid volume. 3 to calculate rectangular parallelepiped. ")
